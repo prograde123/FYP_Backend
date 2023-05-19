77882bb943f1e11a78a88d54e0fe35cbd473f412
khansakiran <Khansakiran2@gmail.com>
2023-05-19 12:16:28 +0500

    push

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,72 +1,63 @@
 var User = require("../models/user");
 var AsyncHandler = require("express-async-handler");
-var generateToken = require("../Utills/generateToken")
-var Teacher = require('../models/teacher')
+var generateToken = require("../Utills/generateToken");
+var Teacher = require("../models/teacher");
 // Register User
-const registerUser = AsyncHandler(
-    async(req, res, next)=>{
-       const {fullName, email, password, role, phoneNum, profilePic} = req.body
-       const emailExist = await User.findOne({email})
+const registerUser = AsyncHandler(async (req, res, next) => {
+  const { fullName, email, password, role, phoneNum, profilePic } = req.body;
+  const emailExist = await User.findOne({ email });
 
-       if(emailExist){
-        res.status(400)
-        throw new Error("Email must be unique");
-       }
-    
-       const newUser = await User.create({fullName, email, password, role, phoneNum, profilePic})
-      
-        if(newUser){
-        res.status(201).json({
-            
-            _id : newUser._id,
-            fullName : newUser.fullName,
-            email : newUser.email,
-            role: newUser.role,
-            phoneNum : newUser.phoneNum,
-            profilePic : newUser.profilePic,
-            //token generate
-            token : generateToken(newUser._id)
-        })
-            }
+  if (emailExist) {
+    res.status(400);
+    throw new Error("Email must be unique");
+  }
 
+  const newUser = await User.create({
+    fullName,
+    email,
+    password,
+    role,
+    phoneNum,
+    profilePic,
+  });
 
-    }
-)
+  if (newUser) {
+    res.status(201).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      email: newUser.email,
+      role: newUser.role,
+      phoneNum: newUser.phoneNum,
+      profilePic: newUser.profilePic,
+      //token generate
+      token: generateToken(newUser._id),
+    });
+  }
+});
 // SignIn user
-const signinUser = AsyncHandler(
-    async(req,res,next)=>{
-        const {email, password} = req.body
-        const userExist = await User.findOne({email})
-        if(!userExist){
-            res.status(401)
-            throw new Error("Invalid Email");
-        }
-        else{
-            if(await userExist.matchPassword(password)){
-                var userid = userExist._id
-                const teacher = await Teacher.findOne({ userid}).exec();
-                res.json({
-                    _id : userExist._id,
-                    fullName : userExist.fullName,
-                    phoneNum : userExist.phoneNum,
-                    profilePic : userExist.profilePic,
-                    email : userExist.email,
-                    role: userExist.role,
-                    teacherID: teacher._id,
-                    teachercv:teacher.cv,
-                    IsApproved:teacher.approved,
-                    //token generate
-                    token : generateToken(userExist._id)
-                })
-            }
-            else{
-                res.status(401)
-            throw new Error("Invalid Password");
-            }
-        }
+const signinUser = AsyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  const userExist = await User.findOne({ email });
+  if (!userExist) {
+    res.status(401);
+    //throw new Error("Invalid Email");
+  } else {
+    if (await userExist.matchPassword(password)) {
+      var userid = userExist._id;
+      const teacher = await Teacher.findOne({ user: userid }).populate('user')
+      console.log(teacher)
+      res.json({
+        teacher: teacher,
+        //token generate
+        token: generateToken(userExist._id),
+      });
+    } else {
+      res.status(401);
+      //throw new Error("Invalid Password");
     }
-)
+  }
+});
 module.exports = {
-    registerUser,
-    signinUser
-}
+  registerUser,
+  signinUser,
+};
