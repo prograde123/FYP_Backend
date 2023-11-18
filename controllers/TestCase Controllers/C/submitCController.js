27c58b5a126919f9bba52f428c49fb55332ffe4a
prograde123 <prograde123@gmail.com>
2023-11-18 15:51:50 +0500

    push

--- controllers/TestCase Controllers/C/submitCController.js
+++ controllers/TestCase Controllers/C/submitCController.js
@@ -24,42 +24,40 @@ var storage = multer.diskStorage({
 var upload = multer({ storage: storage }).array("files", 12);
 
 async function createSubmission(results, req, res, obtainedMarks) {
-  let submission
-    const submissionData = {
-        question: req.params.qid,
-        student: req.user._id,  
-        submittedDate: new Date(),
-        codeFile: req.files[0].originalname,
-        testResults: results,
-        obtainedMarks: Math.round(obtainedMarks,2)
-    };
-    const isReSubmissionRequest= JSON.parse(req.params.isReSubmission)
-    
-    if(isReSubmissionRequest){
-        submission = new resubmit(submissionData)
-    }
-    else{
-        submission = new Submission(submissionData);
-    }
-    try {
-        await submission.save();
-        res.json({ results });
-    } catch (error) {
-        console.error(`Error saving Submission: ${error}`);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
+  let submission;
+  const submissionData = {
+    question: req.params.qid,
+    student: req.user._id,
+    submittedDate: new Date(),
+    codeFile: req.files[0].originalname,
+    testResults: results,
+    obtainedMarks: Math.round(obtainedMarks, 2),
+  };
+  const isReSubmissionRequest = JSON.parse(req.params.isReSubmission);
+
+  if (isReSubmissionRequest) {
+    submission = new resubmit(submissionData);
+  } else {
+    submission = new Submission(submissionData);
+  }
+  try {
+    await submission.save();
+    res.json({ results });
+  } catch (error) {
+    console.error(`Error saving Submission: ${error}`);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
 
 function updateCCode(filePath) {
-  let CCode = fs.readFileSync(filePath, 'utf-8');
+  let CCode = fs.readFileSync(filePath, "utf-8");
   const regex = /printf\("((?![\\n* ]).*)"\)/g;
   javaCode = CCode.replace(regex, function (match, capturedContent) {
-
     return 'printf("")';
   });
-  console.log("updated code",javaCode)
+  console.log("updated code", javaCode);
 
-  fs.writeFileSync(filePath, javaCode, 'utf-8');
+  fs.writeFileSync(filePath, javaCode, "utf-8");
 }
 
 //first scenario
@@ -68,7 +66,7 @@ const uploadC = async (req, res, next) => {
 
   const testCases = await testCase.find({ Question: question });
   const ques = await questionModel.findOne({ _id: question });
-  
+
   const eachTestCaseMarks =
     parseFloat(ques.questionTotalMarks) / parseFloat(testCases.length);
 
@@ -84,8 +82,7 @@ const uploadC = async (req, res, next) => {
         if (err) {
           console.error(`Error reading the file ${file.path}`);
         } else {
-          updateCCode(file.path)
-          
+          updateCCode(file.path);
         }
       });
     });
@@ -98,7 +95,7 @@ const uploadC = async (req, res, next) => {
         createSubmission(results, req, res, obtainedMarks);
         return;
       }
-    
+
       const testCase = testCases[index];
       const dockerExec = spawn("docker", [
         "exec",
@@ -108,39 +105,45 @@ const uploadC = async (req, res, next) => {
         "-c",
         `gcc -o myprogram ${req.files[0].originalname} && ./myprogram`,
       ]);
-    
+
       let actualOutput = ""; // Store the entire output as a single string
       let errorOutput = "";
-    
+
       dockerExec.stdout.on("data", (data) => {
         actualOutput += data.toString();
-        console.log("actual output " , actualOutput)
+        console.log("actual output ", actualOutput);
       });
-    
+
       dockerExec.stderr.on("data", (data) => {
         errorOutput += data.toString();
-        console.log("actual output " , errorOutput)
+        console.log("actual output ", errorOutput);
       });
-    
+
       dockerExec.on("close", (code) => {
         const expectedOutputLines = testCase.output.split("\n"); // Split expected output into lines
         const actualOutputLines = actualOutput.split("\n"); // Split actual output into lines
-        console.log("actualOutputLines " ,actualOutputLines)
+        console.log("actualOutputLines ", actualOutputLines);
         // Remove empty lines from the actual output
-        const cleanedActualOutputLines = actualOutputLines.filter((line) => line.trim() !== "");
-    
-        let passed = errorOutput.trim() === "" && 
+        const cleanedActualOutputLines = actualOutputLines.filter(
+          (line) => line.trim() !== ""
+        );
+
+        let passed =
+          errorOutput.trim() === "" &&
           expectedOutputLines.length === cleanedActualOutputLines.length;
-    
+
         if (passed) {
           for (let i = 0; i < expectedOutputLines.length; i++) {
-            if (expectedOutputLines[i].trim() !== cleanedActualOutputLines[i].trim()) {
+            if (
+              expectedOutputLines[i].trim() !==
+              cleanedActualOutputLines[i].trim()
+            ) {
               passed = false;
               break;
             }
           }
         }
-    
+
         const result = {
           testCase: testCase._id,
           input: testCase.input,
@@ -149,12 +152,12 @@ const uploadC = async (req, res, next) => {
           errorOutput: errorOutput,
           passed: passed,
         };
-    
+
         const testResult = new testCaseResult(result);
         try {
           testResult.save();
           results.push(testResult._id); // Storing the ObjectId in the results array
-    
+
           if (passed) {
             obtainedMarks += eachTestCaseMarks;
           }
@@ -162,13 +165,24 @@ const uploadC = async (req, res, next) => {
         } catch (error) {
           console.error(`Error saving TestResult: ${error}`);
         }
-        
       });
-    
+
       const isInputArray = ques.isInputArray;
-    
+
       if (isInputArray) {
-        const inputBuffer = Buffer.from(testCase.input.map(String).join("\n"));
+        const arraySize = testCase.arraySize;
+        let inputBuffer = "";
+
+        if (Array.isArray(testCase.input)) {
+          inputBuffer = Buffer.from(arraySize + "\n");
+          inputBuffer = Buffer.concat([
+            inputBuffer,
+            Buffer.from(testCase.input.join("\n") + "\n"),
+          ]);
+        } else {
+          inputBuffer = Buffer.from(testCase.input.replace(",", "\n") + "\n");
+        }
+
         dockerExec.stdin.write(inputBuffer);
         dockerExec.stdin.end();
       } else {
@@ -176,27 +190,24 @@ const uploadC = async (req, res, next) => {
         dockerExec.stdin.end();
       }
     }
-    
 
     runTestCase(0);
   });
 };
-
 
 //second scenario
 const getOutputC = async (req, res, next) => {
   const testCasesString = req.params.testCases;
   const isInputArray = req.params.isInputArray;
   const isArr = JSON.parse(isInputArray);
-  
+
   let testCases;
-  
+
   if (isArr) {
     testCases = JSON.parse(testCasesString);
   } else {
     testCases = JSON.parse(testCasesString);
   }
-
 
   upload(req, res, function (err) {
     if (err) {
@@ -208,7 +219,7 @@ const getOutputC = async (req, res, next) => {
         if (err) {
           console.error(`Error reading the file ${file.path}`);
         } else {
-          updateCCode(file.path)
+          updateCCode(file.path);
           console.log(`Content of the file ${file.originalname}:`);
           //console.log(data);
         }
@@ -217,43 +228,46 @@ const getOutputC = async (req, res, next) => {
 
     var codeFilePath = req.files[0].originalname;
 
-    const OutputArray = []
+    const OutputArray = [];
     function runTestCase(index) {
-      
       if (testCases.length <= index) {
-        let inputOutputArray = []
-        for(i=0;i<testCases.length ; i++){
-          const outputString = OutputArray[i].join('\n');
+        let inputOutputArray = [];
+        for (i = 0; i < testCases.length; i++) {
+          const outputString = OutputArray[i].join("\n");
           inputOutputArray.push({
-            input : testCases[i].input,
-            output : outputString
-          })
+            input: testCases[i].input,
+            output: outputString,
+            arraySize : isArr ? testCases[i].arraySize : null
+          });
         }
-        console.log("inputOutput Array is : " , inputOutputArray)
+        console.log("inputOutput Array is : ", inputOutputArray);
         res.send(inputOutputArray);
         return;
       }
-    const testCase = testCases[index].input;
-    console.log("TEst CASE input " , testCase)
+      const testCase = testCases[index].input;
+      console.log("TEst CASE input ", testCase);
 
-    const dockerExec = spawn("docker", [
-      "exec",
-      "-i",
-      "c-ccomp-1",
-      "sh",
-      "-c",
-      `gcc -o myprogram ${req.files[0].originalname} && ./myprogram`,
-    ]);
+      const dockerExec = spawn("docker", [
+        "exec",
+        "-i",
+        "c-ccomp-1",
+        "sh",
+        "-c",
+        `gcc -o myprogram ${req.files[0].originalname} && ./myprogram`,
+      ]);
 
       let actualOutput = "";
       let errorOutput = "";
 
       dockerExec.stdout.on("data", (data) => {
-        actualOutput = data.toString().split('\n');
+        actualOutput = data.toString().split("\n");
         OutputArray[index] = OutputArray[index] || [];
         OutputArray[index].push(...actualOutput);
         console.log(OutputArray);
-        if (OutputArray[index].length > 0 && OutputArray[index][OutputArray[index].length - 1] === '') {
+        if (
+          OutputArray[index].length > 0 &&
+          OutputArray[index][OutputArray[index].length - 1] === ""
+        ) {
           // Remove the last empty line
           OutputArray[index].pop();
         }
@@ -268,14 +282,30 @@ const getOutputC = async (req, res, next) => {
       });
 
       if (isArr) {
-        const inputBuffer = Buffer.from(testCase.map(String).join("\n"));
+        const arraySize = testCase.arraySize;
+        let inputBuffer = '';
+
+        if (Array.isArray(testCase.input)) {
+          inputBuffer = Buffer.from(arraySize + "\n");
+          inputBuffer = Buffer.concat([
+            inputBuffer,
+            Buffer.from(testCase.input.map(String).join("\n") + "\n"),
+          ]);
+        } else if (testCase.input) {
+          inputBuffer = Buffer.from(testCase.input.replace(",", "\n") + "\n");
+        }
+
         dockerExec.stdin.write(inputBuffer);
-      } else {
-        dockerExec.stdin.write(testCase.replace(",", "\n"));  
+        dockerExec.stdin.end();
+      } 
+      
+      else {
+        dockerExec.stdin.write(testCase.replace(",", "\n"));
+        dockerExec.stdin.end();
       }
-      dockerExec.stdin.end();
+      
     }
     runTestCase(0);
   });
 };
-module.exports = { uploadC , getOutputC };
+module.exports = { uploadC, getOutputC };
